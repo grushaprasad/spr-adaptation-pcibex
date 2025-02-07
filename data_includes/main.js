d2b1d402628b6ee67fb082926b83f030cf1fb3a4
Emily Morgan <51768061+eimorgan@users.noreply.github.com>
2025-02-07 14:07:51 -0800

    Include end of expt link to confirm SONA participation

--- data_includes/main.js
+++ data_includes/main.js
@@ -231,11 +231,35 @@ PennController("feed_back_request",
 //Sending Results
 PennController.SendResults("send_results").setOption("countsForProgressBar",false);
 //Ending the Experiment
-newTrial("end_of_exp",
-    newHtml("end_of_exp", "end_of_exp.html")
-        .settings.log()
-        .print()
-    ,
-    newButton().wait(getHtml("end_of_exp").test.complete()
-            .failure(getHtml("end_of_exp").warn()))
+//newTrial("end_of_exp",
+//    newHtml("end_of_exp", "end_of_exp.html")
+//        .settings.log()
+//        .print()
+//    ,
+//    newButton().wait(getHtml("end_of_exp").test.complete()
+//            .failure(getHtml("end_of_exp").warn()))
+//).setOption("countsForProgressBar",false);
+newTrial( "final" ,
+     newText("<p>Thank you for your participation!</p>")
+          .center()
+          .print()
+     ,
+     newText("<p><a href='https://ucdavis.sona-systems.com/webstudy_credit.aspx?experiment_id=3675&credit_token=9720da8a96144b5b829464e000827548&survey_code="+GetURLParameter("id")+"' target='_blank'>Click here to confirm your participation on SONA.</a></p> <p>This is a necessary step in order for you to receive participation credit!</p>")
+          .center()
+          .print()
+     ,
+     newText("<p>This experiment was a part of a group of experiments looking at <i> syntactic adaptation </i> for garden path sentences. Garden path sentences are grammatically correct sentences where the reader starts off with a likley interpretation of the sentence but has to revaulate this interpretation at a later point in the sentence. For example consider the sentence:<blockquote> (1) The soldiers warned about the dangers conducted the midnight raid </blockquote>In this sentence we start off by thinking that the soldiers were the ones who were warning about the dangers. However when we get to 'conducted' this interpretation is no longer plausible. In order for the sentence to make sense, we have to reintepret it as meaning that the soldiers <i> who were </i> warned about the dangers were the ones conducting the midnight raid. </p>")
+          .center()
+          .print()
+     ,
+     newText("<p>People tend to take longer to read ambiguous garden path sentences ('The soldiers warned about the dangers ...') than the unambiguous non-garden path sentences ('The soldiers <i> who were </i> were warned about the dangers ...'). This difference in reading times is referred to as the <i> garden path effect </i>. The main question that we were interested in answering with these group of experiments is whether we can alter the frequencies of different kinds of sentences to reduce this garden path effect. In other words can we get people to start expecting sentences like (1) more either by showing them more sentences like (1), other sentences with relative clauses (ex. 'The pizza <i> that I ate yesterday </i> was delicious') or other unexpected sentences?</p>")
+          .center()
+          .print()
+     ,
+     newText("<p>After you have followed the link above to receive your SONA credit, you can close this Browser tab.  Thanks again for participating in the experiment!</p>")
+          .center()
+          .print()
+     ,
+     newButton("void")
+          .wait()
 ).setOption("countsForProgressBar",false);
